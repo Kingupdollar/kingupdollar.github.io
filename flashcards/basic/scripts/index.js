518995216701f89ab1ff6ddf176519b76dad4354
Kingupdollar <42480845+Kingupdollar@users.noreply.github.com>
2021-06-29 04:06:42 +0800

    Create index.js

---
+++ flashcards/basic/scripts/index.js
@@ -0,0 +1,22 @@
+let questions = [
+  {
+  "question": "Question 1: What is the highest building in Taiwan?",
+  "answer": "Answer: Taipei 101",
+  },
+  {
+  "question": "Question 2: What is the capital city of Taiwan?",
+  "answer": "Answer: Taipei City",
+  },
+  {
+  "question": "Question 3: How much is Taiwan's population?",
+  "answer": "Answer: 23 million",
+  }
+];
+
+for (i=0; i<questions.length; i++){
+  alert(questions[i].question);
+  alert(questions[i].answer);
+  if (i=questions.length-1){
+    i=0;
+  }
+}
