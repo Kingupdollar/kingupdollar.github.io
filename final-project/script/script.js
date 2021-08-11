b453f5384e1653bae5e210d2406f8e4c4eea5b38
Kingupdollar <42480845+Kingupdollar@users.noreply.github.com>
2021-08-11 03:51:02 -0400

    Update script.js

--- final-project/script/script.js
+++ final-project/script/script.js
@@ -1,18 +1,24 @@
+//feedback for question 1
 function correctFeedback1(event) {
   var feedback = document.getElementById('feedback1');
   feedback.innerHTML = '<h3 class="feedbackCorrect">✓ Good job!</h3>'
 }
 
+//feedback for question 1
 function incorrectFeedback1(event) {
   var feedback = document.getElementById('feedback1');
   feedback.innerHTML = '<h3 class="feedbackInorrect">✗ Try again!</h3>'
 }
 
+
+
+
+//feedback for question 2
 function correctFeedback2(event) {
   var feedback = document.getElementById('feedback2');
   feedback.innerHTML = '<h3 class="feedbackCorrect">✓ Good job!</h3>'
 }
-
+//feedback for question 2
 function incorrectFeedback2(event) {
   var feedback = document.getElementById('feedback2');
   feedback.innerHTML = '<h3 class="feedbackInorrect">✗ Try again!</h3>'
